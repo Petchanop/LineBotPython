9f064b25a653b3f48f7ae432435421b3f1225e58
petchanop <nop.hide@gmail.com>
2025-03-27 17:28:30 +0700

    fix line notify import

--- lineBot/lineBot/asgi.py
+++ lineBot/lineBot/asgi.py
@@ -36,7 +36,7 @@ app.add_middleware(
 )
 
 def init(app: FastAPI):
-    from linenotify.routes.routes import router as linenotify_router
+    from lineBot.linenotify.routes.routes import router as linenotify_router
     from lineBot.lineBot.urls import api_router
 
     api_router.include_router(linenotify_router)
