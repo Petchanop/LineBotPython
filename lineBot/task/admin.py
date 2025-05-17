94ca6545fcaddc11d38a9baddc64b18c57a3cb8c
petchanop <nop.hide@gmail.com>
2025-05-17 17:36:38 +0700

    add task model to django admin

--- lineBot/task/admin.py
+++ lineBot/task/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from lineBot.task.models import Task
+
 # Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'contact_id', 'name', 'description', 'created_at', 'updated_at']
+
+admin.site.register(Task, TaskAdmin)
+
